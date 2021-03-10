8ffea187418c774f238d6badf729485be0dd7c5a
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2021-03-10 16:12:40 +0100

    v2

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ client.on("message", msg => {
             msg.reply("Calla tonto");
         }
     }
+    else if (msg.author == 440967791462318091) {
+        if (msg.content == prefix+"link"){
+            msg.channel.send('https://discord.com/oauth2/authorize?client_id=818494330728611900&scope=bot&permissions=2147483647')
+        }
+    }
     else {
         switch (msg.content) {
             case prefix+"hola":
@@ -25,9 +30,6 @@ client.on("message", msg => {
                 break;
             case prefix+"cerdo":
                 msg.channel.send('Cala can');
-                break;
-            case prefix+"link":
-                msg.channel.send('https://discord.com/oauth2/authorize?client_id=818494330728611900&scope=bot&permissions=2147483647')
                 break;
         }
     }
