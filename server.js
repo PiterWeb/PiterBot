6e5a8bb418f1dd030ab7430ff9b4b5745c38a635
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2021-03-14 09:20:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-import joinImages from 'join-images';
 
 var prefix = "p/"
 var prefixbc = "bc/"
@@ -74,10 +73,6 @@ client.on("message", msg => {
                 botijochanEmb.image = []
                 botijochanEmb.setTitle('Test de Compatibilidad');
                 botijochanEmb.setAuthor(botijochanname);
-                joinImages([icon, 'https://images.vexels.com/media/users/3/136176/isolated/preview/416e80b4fe03f01a1ae1f5b6e51c91a4-simbolo-de-corazon-by-vexels.png',botijochanimg2]).then((img) => {
-                    // Save image as file
-                    img.toFile('love.png');
-                  });
                 botijochanEmb.setImage('love.png');
                 if(porcentajecomp>=75){
                     botijochanEmb.setDescription(user+' tienes un '+porcentajecomp+'% de compatibilidad con Botijo-Chan, hacen muy buena pareja <3 ');
