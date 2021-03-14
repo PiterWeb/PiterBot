c9c490227cc76f894bc534a58c22c3fb0189e74a
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2021-03-14 22:01:38 +0100

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -8,5 +8,37 @@ module.exports = async () => {
         useUnifiedTopology: true,
     })
 
+    const kittySchema = new mongoose.Schema({
+        name: String
+    });
+    
+    const Kitten = mongoose.model('Kitten', kittySchema);
+
+    const silence = new Kitten({ name: 'Silence' });
+    console.log(silence.name); // 'Silence'
+
+    kittySchema.methods.speak = function () {
+        const greeting = this.name
+          ? "Meow name is " + this.name
+          : "I don't have a name";
+        console.log(greeting);
+      }
+      
+      const fluffy = new Kitten({ name: 'fluffy' });
+      fluffy.speak();
+
+      fluffy.save(function (err, fluffy) {
+        if (err) return console.error(err);
+        fluffy.speak();
+      });
+
+      Kitten.find(function (err, kittens) {
+        if (err) return console.error(err);
+        console.log(kittens);
+      });
+
+      Kitten.find({ name: /^fluff/ }, callback);
+
     return mongoose;
+
 }
