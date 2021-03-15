059ca3d4c27003b695ee0df972fa2a886a87646c
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2021-03-15 20:55:41 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,6 @@ const mysql = require('mysql');
 
 const con = mysql.createConnection({
     host: process.env.HOST,
-    port: process.env.PORT,
     user: process.env.USER,
     password: process.env.PSW,
     database: process.env.DB,
@@ -11,8 +10,8 @@ const con = mysql.createConnection({
   con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
-    con.query(sql, function (err, result) {
+    var sqluser = "CREATE TABLE users (name VARCHAR(255), messages VARCHAR(255))";
+    con.query(sqluser, function (err, result) {
         if (err) throw err;
         console.log("Table created");
       });
