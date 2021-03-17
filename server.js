92b6bb6813f226af011a69443e0af0a8000af69f
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2021-03-17 15:50:55 +0100

    sql

--- server.js
+++ server.js
@@ -3,25 +3,16 @@ const client = new Discord.Client();
 const sql = require('./modules/db')
 const mysql = require('mysql');
 
-const con = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PSW,
-    database: process.env.DB,
-  });
-
-
 var prefix = "p/"
 var prefixbc = "bc/"
 
-const mysql = require('mysql');
-
 const con = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PSW,
     database: process.env.DB,
   });
+
 
   con.connect(function(err) {
     if (err) throw err;
