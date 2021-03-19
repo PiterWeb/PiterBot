c8e93bacbd1d9a5c728cb18f4e8bca0d724f624e
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2021-03-19 11:03:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ client.on("message", msg => {
     const command = args.shift().toLowerCase();
 
         switch (msg.content) {
-            case prefix+"name"+:
+            case prefix+"name":
                 var task = 'INSERT INTO users (name) VALUES ('+args+')';
                 msg.channel.send(`Has cambiado exitosamente el nombre con el que te reconoce el bot a ${args}`);
                 db.sql(task);
