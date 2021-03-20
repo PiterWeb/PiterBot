dc1beff81bfa0538e5f087640a2e23bad43050df
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2021-03-20 09:53:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ client.on("message", msg => {
                       .then(msg => {
                         msg = msg.first()
                         
-                        var task = "INSERT INTO users(name) VALUES('"+msg+"')";
+                        var task = "INSERT INTO users(name,user) VALUES("+msg+","+user+")";
                         msg.channel.send(`Has cambiado exitosamente el nombre con el que te reconoce el bot a ${msg}`);
                         db.sql(task);
                         })
