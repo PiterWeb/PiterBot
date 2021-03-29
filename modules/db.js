451e78d2a1bc48f1d44af02ffb8d841392c20026
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2021-03-29 22:40:34 +0200

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -7,40 +7,31 @@ const con = mysql.createConnection({
     database: process.env.DB,
   });
 
+  con.connect(function(err) {
+    if (err) return console.error(err.message);
+    console.log("Connected!");
+      });
+
 //   con.connect(function(err) {
 //     if (err) throw err;
 //     console.log("Connected!");
 //         var sqluserins = "INSERT INTO users (name) VALUES ('PiterZ')";
 
       function sqlinsert(task){
-        con.connect(function(err) {
-          if (err) return console.error(err.message);
-          console.log("Connected!");
-          
-            });
         con.query(task, function (err, result) {
           if (err) return console.error(err.message);
           console.log(result);
-          
         });
-        con.end();
       } 
 
       function sqlselect(task, callback){
-        con.connect(function(err) {
-          if (err) return console.error(err.message);
-          console.log("Connected!");
-          
-            });
         con.query(task, function (err, result , fields) {
           if (err) {
             return console.error(err.message);
           } else {
             callback(null,result[0].name);
           }
-          
         });
-        con.end();
       }   
 
   module.exports = {sqlinsert , sqlselect};
